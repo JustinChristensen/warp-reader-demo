a02cddbc2a6c8870c54e6e88148ea702e69b1475
Justin Christensen <wroathe@gmail.com>
2019-01-08 19:43:59 -0600

    select app via options

--- Main.hs
+++ Main.hs
@@ -1,10 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import qualified Data.Map as M
-import System.Environment (getEnvironment)
+import System.Exit (exitFailure)
+import System.Environment (getEnvironment, getArgs, getProgName)
 import Control.Monad.Reader
 import Control.Monad.Identity
 import Data.Maybe (fromMaybe)
+import Data.List (uncons)
+import qualified Data.Text as T
+import qualified Data.ByteString.Lazy.Char8 as C
+
+import Network.Wai (Application, pathInfo, responseLBS)
+import Network.Wai.Handler.Warp (
+    defaultSettings,
+    setHost,
+    setBeforeMainLoop,
+    setServerName,
+    runSettings)
+import Network.HTTP.Types (status200, status404)
 
 type Env = M.Map String String
 type SimpleReader = ReaderT Env Identity
@@ -36,7 +50,51 @@ ioReaderTest = do
     env <- getEnvMap
     runReaderT readAndGreet env
 
+helloApp :: Application
+helloApp req res = let
+        parts = pathInfo req
+        greeting str = C.pack $ "Hello " ++ T.unpack str ++ "!\n"
+        send404 = res $ responseLBS status404 [] ""
+    in
+        fromMaybe send404 $ uncons parts >>= \("greet", rest) ->
+            uncons rest >>= \(str, []) ->
+                return $ res $ responseLBS status200 []  $ greeting str
+
+runHelloApp :: IO ()
+runHelloApp = let
+            preamble = putStrLn "HelloApp listening..."
+            settings =
+                setHost "!4" $
+                setServerName "" $
+                setBeforeMainLoop preamble
+                defaultSettings
+    in runSettings settings helloApp
+
 main :: IO ()
-main = do
-    simpleReaderTest
-    ioReaderTest
+main = let
+        apps = [
+            ("simpleReaderTest", simpleReaderTest),
+            ("ioReaderTest", ioReaderTest),
+            ("runHelloApp", runHelloApp)]
+        usage = do
+            name <- getProgName
+            putStrLn $ "usage: " ++ name ++ " [options]"
+            putStrLn "-h\tPrint help"
+            putStrLn "-N\tRun application #N"
+            putStrLn "\nChoices:"
+            putStrLn $ unlines $ 
+                zipWith (\i a -> show i ++ ". " ++ a) [1..] $ map fst apps
+    in do
+        args <- getArgs
+        if "-h" `elem` args then usage
+        else case uncons args of
+            Just (s, _) -> do
+                let app = pred $ read $ tail s :: Int
+                if app `elem` [0..pred $ length apps] then
+                    snd $ apps !! app
+                else do
+                    usage
+                    exitFailure
+            _ -> do
+                usage
+                exitFailure
